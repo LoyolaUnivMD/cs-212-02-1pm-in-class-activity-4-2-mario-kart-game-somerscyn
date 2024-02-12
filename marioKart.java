3d7e9ca3dc3eb83f628606c146924d5a0bedebd5
somerscyn <144164003+somerscyn@users.noreply.github.com>
2024-02-12 10:57:45 -0500

    Update marioKart.java

--- marioKart.java
+++ marioKart.java
@@ -1,12 +1,58 @@
-// Read the README.md
-// Read it again
-// Your code here
-// Delete these 4 lines of comments
+
 
 class HelloWorld {
-    public static void main(String[] args) {
+public static class MarioKartPlayer {
+    
 
-        System.out.println("Mario Kart!");
+        // data fields
+        private String playerName;
+        private String character;
+        private int speed;
         
+        // general constructor (parameterized)
+        public MarioKartPlayer(String playerName, String character, int speed){
+            this.playerName = playerName;
+            this.character = character;
+            this.speed = speed;
+        }
+        
+        // boost method 
+        public void boostSpeed(){
+            speed += 10;}
+        
+        // display info method 
+        public void displayInfo(){
+            System.out.println("Name: " + playerName);
+            System.out.println("Character: " + character);
+            System.out.println("Speed: " + speed);
+        }
+
     }
+
+public static class MarioKartGame {
+    public static void main(String[] args) {
+  
+    // creating an object aka a character here 
+    MarioKartPlayer bob = new MarioKartPlayer("Bob", "Hedgehog", 15);
+    MarioKartPlayer joe = new MarioKartPlayer("Joe", "Mouse", 20);
+    
+    // display info
+    bob.displayInfo();
+    joe.displayInfo();
+    
+    // simulate a boost for each 
+    bob.boostSpeed();
+    joe.boostSpeed();
+    
+    // display updated info
+    bob.displayInfo();
+    joe.displayInfo();
+    
+    
+    }}
+    
+    
+    
+    
+    
 }
